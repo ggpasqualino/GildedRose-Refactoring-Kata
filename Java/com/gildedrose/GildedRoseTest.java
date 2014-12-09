f82b55baaa17bbe2c43f58ef9b1bf1ca7f98c13d
Guilherme <ggpasqualino@gmail.com>
2014-12-09 23:07:26 +0100

    Adds tests

--- Java/com/gildedrose/GildedRoseTest.java
+++ Java/com/gildedrose/GildedRoseTest.java
@@ -15,14 +15,17 @@ public class GildedRoseTest {
 
 	@Test
 	public void notLegendaryItemHasToBeSold() throws Exception {
-		final Item[] items = new Item[] { genericItemNotOverdueWithQuality(0) };
+		final Item[] items = new Item[] { genericItem(0), agedBrie(0),
+				backstagePass(20, 0) };
 		updateQuality(items);
 		assertEquals(19, items[0].sellIn);
+		assertEquals(19, items[1].sellIn);
+		assertEquals(19, items[2].sellIn);
 	}
 
 	@Test
-	public void qualityDoesNotGoBelowZero() throws Exception {
-		final Item[] items = new Item[] { genericItemNotOverdueWithQuality(0) };
+	public void qualityDoesNotGoBelowZeroForGenericItem() throws Exception {
+		final Item[] items = new Item[] { genericItem(0) };
 		updateQuality(items);
 		assertEquals(0, items[0].quality);
 	}
@@ -36,9 +39,108 @@ public class GildedRoseTest {
 
 	@Test
 	public void qualityDecreasesByOneForGenericItem() throws Exception {
-		final Item[] items = new Item[] { genericItemNotOverdueWithQuality(10) };
+		final Item[] items = new Item[] { genericItem(10) };
 		updateQuality(items);
 		assertEquals(9, items[0].quality);
+	}
+
+	@Test
+	public void qualityDoesNotGoAboveFifty() throws Exception {
+		final Item[] items = new Item[] { agedBrie(50),
+				backstagePass(20, 50) };
+		updateQuality(items);
+		assertEquals(50, items[0].quality);
+		assertEquals(50, items[1].quality);
+	}
+
+	@Test
+	public void qualityIncreasesByOneForAgedBrie() throws Exception {
+		final Item[] items = new Item[] { agedBrie(10) };
+		updateQuality(items);
+		assertEquals(11, items[0].quality);
+	}
+
+	@Test
+	public void qualityIncreasesByOneForBackstageWhenSellInGreaterOrEqualsEleven()
+			throws Exception {
+		final Item[] items = new Item[] { backstagePass(11, 10) };
+		updateQuality(items);
+		assertEquals(11, items[0].quality);
+	}
+
+	@Test
+	public void qualityIncreasesByTwoForBackstageWhenSellInGreaterOrEqualsSixAndLesserThanEleven()
+			throws Exception {
+		final Item[] items = new Item[] { backstagePass(10, 10) };
+		updateQuality(items);
+		assertEquals(12, items[0].quality);
+	}
+
+	@Test
+	public void qualityDoesNotGoAboveFifityForBackstageWhenSellInGreaterOrEqualsSixAndLesserThanEleven()
+			throws Exception {
+		final Item[] items = new Item[] { backstagePass(10, 49) };
+		updateQuality(items);
+		assertEquals(50, items[0].quality);
+	}
+
+	@Test
+	public void qualityIncreasesByThreeForBackstageWhenSellInGreaterOrEqualsZeroAndLesserThanSix()
+			throws Exception {
+		final Item[] items = new Item[] { backstagePass(5, 10) };
+		updateQuality(items);
+		assertEquals(13, items[0].quality);
+	}
+
+	@Test
+	public void qualityDoesNotGoAboveFifityForBackstageWhenSellInGreaterOrEqualsZeroAndLesserThanSix()
+			throws Exception {
+		final Item[] items = new Item[] { backstagePass(5, 48) };
+		updateQuality(items);
+		assertEquals(50, items[0].quality);
+	}
+
+	@Test
+	public void qualityIncreasesByTwoForOverdueAgedBrie() throws Exception {
+		final Item[] items = new Item[] { overdueAgedBrie(10) };
+		updateQuality(items);
+		assertEquals(12, items[0].quality);
+	}
+
+	@Test
+	public void qualityDoesNotGoAboveFiftyForOverdueAgedBrie() throws Exception {
+		final Item[] items = new Item[] { overdueAgedBrie(50) };
+		updateQuality(items);
+		assertEquals(50, items[0].quality);
+	}
+
+	@Test
+	public void qualityDropsToZeroForOverdueBackstagePass() throws Exception {
+		final Item[] items = new Item[] { overdueBackstagePass() };
+		updateQuality(items);
+		assertEquals(0, items[0].quality);
+	}
+
+	@Test
+	public void qualityDoesNotChangeForOverdueLegendaryItem() throws Exception {
+		final Item[] items = new Item[] { overdueLegendaryItem() };
+		updateQuality(items);
+		assertEquals(10, items[0].quality);
+	}
+
+	@Test
+	public void qualityDecreasesByTwoForOverdueGenericItem() throws Exception {
+		final Item[] items = new Item[] { overdueGenericItem(10) };
+		updateQuality(items);
+		assertEquals(8, items[0].quality);
+	}
+
+	@Test
+	public void qualityDoesNotGoBelowZeroForOverdueGenericItem()
+			throws Exception {
+		final Item[] items = new Item[] { overdueGenericItem(0) };
+		updateQuality(items);
+		assertEquals(0, items[0].quality);
 	}
 
 	private GildedRose updateQuality(final Item[] items) {
@@ -47,11 +149,36 @@ public class GildedRoseTest {
 		return app;
 	}
 
+	private Item backstagePass(final int sellIn, final int quality) {
+		return new Item("Backstage passes to a TAFKAL80ETC concert", sellIn,
+				quality);
+	}
+
+	private Item overdueBackstagePass() {
+		return backstagePass(-1, 10);
+	}
+
 	private Item legendaryItem() {
 		return new Item("Sulfuras, Hand of Ragnaros", 20, 10);
 	}
 
-	private Item genericItemNotOverdueWithQuality(final int quality) {
+	private Item overdueLegendaryItem() {
+		return new Item("Sulfuras, Hand of Ragnaros", -1, 10);
+	}
+
+	private Item genericItem(final int quality) {
 		return new Item("generic item", 20, quality);
+	}
+
+	private Item overdueGenericItem(final int quality) {
+		return new Item("generic item", -1, quality);
+	}
+
+	private Item agedBrie(final int quality) {
+		return new Item("Aged Brie", 20, quality);
+	}
+
+	private Item overdueAgedBrie(final int quality) {
+		return new Item("Aged Brie", -1, quality);
 	}
 }
