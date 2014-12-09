fb5b4c5fe9451e064e1bf3be1a92de469b57f6c1
unknown <pasqualinog@LATITUDEE6530-SSD1.ITWAREKFT.local>
2014-12-09 17:31:32 +0100

    Adds five tests

--- Java/com/gildedrose/GildedRoseTest.java
+++ Java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,57 @@
 package com.gildedrose;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 public class GildedRoseTest {
 
-    @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
-    }
+	@Test
+	public void legendaryItemDoesNotHaveToBeSold() throws Exception {
+		final Item[] items = new Item[] { legendaryItem() };
+		updateQuality(items);
+		assertEquals(20, items[0].sellIn);
+	}
 
+	@Test
+	public void notLegendaryItemHasToBeSold() throws Exception {
+		final Item[] items = new Item[] { genericItemNotOverdueWithQuality(0) };
+		updateQuality(items);
+		assertEquals(19, items[0].sellIn);
+	}
+
+	@Test
+	public void qualityDoesNotGoBelowZero() throws Exception {
+		final Item[] items = new Item[] { genericItemNotOverdueWithQuality(0) };
+		updateQuality(items);
+		assertEquals(0, items[0].quality);
+	}
+
+	@Test
+	public void qualityDoesNotChangeForLegendaryItem() throws Exception {
+		final Item[] items = new Item[] { legendaryItem() };
+		updateQuality(items);
+		assertEquals(10, items[0].quality);
+	}
+
+	@Test
+	public void qualityDecreasesByOneForGenericItem() throws Exception {
+		final Item[] items = new Item[] { genericItemNotOverdueWithQuality(10) };
+		updateQuality(items);
+		assertEquals(9, items[0].quality);
+	}
+
+	private GildedRose updateQuality(final Item[] items) {
+		final GildedRose app = new GildedRose(items);
+		app.updateQuality();
+		return app;
+	}
+
+	private Item legendaryItem() {
+		return new Item("Sulfuras, Hand of Ragnaros", 20, 10);
+	}
+
+	private Item genericItemNotOverdueWithQuality(final int quality) {
+		return new Item("generic item", 20, quality);
+	}
 }
