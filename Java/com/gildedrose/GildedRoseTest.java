54a202753b82f039c3805e7ade20d1c1bde19037
Guilherme <ggpasqualino@gmail.com>
2014-12-10 20:18:15 +0100

    Refactoring of tests
    Extracts some constants
    
    Change method signature "private GildedRose updateQuality(Item[])" to
    "private void updateQuality(Item...)"

--- Java/com/gildedrose/GildedRoseTest.java
+++ Java/com/gildedrose/GildedRoseTest.java
@@ -6,172 +6,194 @@ import org.junit.Test;
 
 public class GildedRoseTest {
 
+    private static final String BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert";
+    private static final String LEGENDARY_ITEM = "Sulfuras, Hand of Ragnaros";
+    private static final String GENERIC_ITEM = "generic item";
+    private static final String AGED_BRIE = "Aged Brie";
+    private static final int SELL_IN = 20;
+    private static final int SELL_IN_OVERDUE = -1;
+    private static final int MAX_QUALITY = 50;
+    private static final int MIN_QUALITY = 0;
+    private static final int DEFAULT_QUALITY = 10;
+
     @Test
     public void legendaryItemDoesNotHaveToBeSold() {
-        final Item[] items = new Item[] { legendaryItem() };
-        updateQuality(items);
-        assertEquals(20, items[0].sellIn);
+        final Item legendaryItem = legendaryItem();
+        updateQuality(legendaryItem);
+        assertEquals(SELL_IN, legendaryItem.sellIn);
     }
 
     @Test
     public void notLegendaryItemHasToBeSold() {
-        final Item[] items = new Item[] { genericItem(0), agedBrie(0),
-                backstagePass(20, 0) };
-        updateQuality(items);
-        assertEquals(19, items[0].sellIn);
-        assertEquals(19, items[1].sellIn);
-        assertEquals(19, items[2].sellIn);
+        final Item genericItem = genericItem(MIN_QUALITY);
+        final Item agedBrie = agedBrie(MIN_QUALITY);
+        final Item backstagePass = backstagePass(SELL_IN, MIN_QUALITY);
+
+        updateQuality(genericItem, agedBrie, backstagePass);
+
+        final int expectedSellIn = SELL_IN - 1;
+        assertEquals(expectedSellIn, genericItem.sellIn);
+        assertEquals(expectedSellIn, agedBrie.sellIn);
+        assertEquals(expectedSellIn, backstagePass.sellIn);
     }
 
     @Test
     public void qualityDoesNotGoBelowZeroForGenericItem() {
-        final Item[] items = new Item[] { genericItem(0) };
-        updateQuality(items);
-        assertEquals(0, items[0].quality);
+        final Item genericItem = genericItem(MIN_QUALITY);
+        updateQuality(genericItem);
+        assertEquals(MIN_QUALITY, genericItem.quality);
     }
 
     @Test
     public void qualityDoesNotChangeForLegendaryItem() {
-        final Item[] items = new Item[] { legendaryItem() };
-        updateQuality(items);
-        assertEquals(10, items[0].quality);
+        final Item legendaryItem = legendaryItem();
+        updateQuality(legendaryItem);
+        assertEquals(DEFAULT_QUALITY, legendaryItem.quality);
     }
 
     @Test
     public void qualityDecreasesByOneForGenericItem() {
-        final Item[] items = new Item[] { genericItem(10) };
-        updateQuality(items);
-        assertEquals(9, items[0].quality);
+        final Item genericItem = genericItem(DEFAULT_QUALITY);
+        updateQuality(genericItem);
+        final int expectedQuality = DEFAULT_QUALITY - 1;
+        assertEquals(expectedQuality, genericItem.quality);
     }
 
     @Test
     public void qualityDoesNotGoAboveFifty() {
-        final Item[] items = new Item[] { agedBrie(50), backstagePass(20, 50) };
-        updateQuality(items);
-        assertEquals(50, items[0].quality);
-        assertEquals(50, items[1].quality);
+        final Item agedBrie = agedBrie(MAX_QUALITY);
+        final Item backstagePass = backstagePass(SELL_IN, MAX_QUALITY);
+
+        updateQuality(agedBrie, backstagePass);
+
+        assertEquals(MAX_QUALITY, agedBrie.quality);
+        assertEquals(MAX_QUALITY, backstagePass.quality);
     }
 
     @Test
     public void qualityIncreasesByOneForAgedBrie() {
-        final Item[] items = new Item[] { agedBrie(10) };
-        updateQuality(items);
-        assertEquals(11, items[0].quality);
+        final Item agedBrie = agedBrie(DEFAULT_QUALITY);
+        updateQuality(agedBrie);
+        final int expectedQuality = DEFAULT_QUALITY + 1;
+        assertEquals(expectedQuality, agedBrie.quality);
     }
 
     @Test
     public void qualityIncreasesByOneForBackstageWhenSellInGreaterOrEqualsEleven() {
-        final Item[] items = new Item[] { backstagePass(11, 10) };
-        updateQuality(items);
-        assertEquals(11, items[0].quality);
+        final Item backstagePass = backstagePass(11, DEFAULT_QUALITY);
+        updateQuality(backstagePass);
+        final int expectedQuality = DEFAULT_QUALITY + 1;
+        assertEquals(expectedQuality, backstagePass.quality);
     }
 
     @Test
     public void qualityIncreasesByTwoForBackstageWhenSellInGreaterOrEqualsSixAndLesserThanEleven() {
-        final Item[] items = new Item[] { backstagePass(10, 10) };
-        updateQuality(items);
-        assertEquals(12, items[0].quality);
+        final Item backstagePass = backstagePass(10, DEFAULT_QUALITY);
+        updateQuality(backstagePass);
+        final int expectedQuality = DEFAULT_QUALITY + 2;
+        assertEquals(expectedQuality, backstagePass.quality);
     }
 
     @Test
     public void qualityDoesNotGoAboveFifityForBackstageWhenSellInGreaterOrEqualsSixAndLesserThanEleven() {
-        final Item[] items = new Item[] { backstagePass(10, 49) };
-        updateQuality(items);
-        assertEquals(50, items[0].quality);
+        final Item backstagePass = backstagePass(10, 49);
+        updateQuality(backstagePass);
+        assertEquals(MAX_QUALITY, backstagePass.quality);
     }
 
     @Test
     public void qualityIncreasesByThreeForBackstageWhenSellInGreaterOrEqualsZeroAndLesserThanSix() {
-        final Item[] items = new Item[] { backstagePass(5, 10) };
-        updateQuality(items);
-        assertEquals(13, items[0].quality);
+        final Item backstagePass = backstagePass(5, DEFAULT_QUALITY);
+        updateQuality(backstagePass);
+        final int expectedQuality = DEFAULT_QUALITY + 3;
+        assertEquals(expectedQuality, backstagePass.quality);
     }
 
     @Test
     public void qualityDoesNotGoAboveFifityForBackstageWhenSellInGreaterOrEqualsZeroAndLesserThanSix() {
-        final Item[] items = new Item[] { backstagePass(5, 48) };
-        updateQuality(items);
-        assertEquals(50, items[0].quality);
+        final Item backstagePass = backstagePass(5, 48);
+        updateQuality(backstagePass);
+        assertEquals(MAX_QUALITY, backstagePass.quality);
     }
 
     @Test
     public void qualityIncreasesByTwoForOverdueAgedBrie() {
-        final Item[] items = new Item[] { overdueAgedBrie(10) };
-        updateQuality(items);
-        assertEquals(12, items[0].quality);
+        final Item overdueAgedBrie = overdueAgedBrie(DEFAULT_QUALITY);
+        updateQuality(overdueAgedBrie);
+        final int expectedQuality = DEFAULT_QUALITY + 2;
+        assertEquals(expectedQuality, overdueAgedBrie.quality);
     }
 
     @Test
     public void qualityDoesNotGoAboveFiftyForOverdueAgedBrie() {
-        final Item[] items = new Item[] { overdueAgedBrie(50) };
-        updateQuality(items);
-        assertEquals(50, items[0].quality);
+        final Item overdueAgedBrie = overdueAgedBrie(MAX_QUALITY);
+        updateQuality(overdueAgedBrie);
+        assertEquals(MAX_QUALITY, overdueAgedBrie.quality);
     }
 
     @Test
     public void qualityDropsToZeroForOverdueBackstagePass() {
-        final Item[] items = new Item[] { overdueBackstagePass() };
-        updateQuality(items);
-        assertEquals(0, items[0].quality);
+        final Item overdueBackstagePass = overdueBackstagePass();
+        updateQuality(overdueBackstagePass);
+        assertEquals(MIN_QUALITY, overdueBackstagePass.quality);
     }
 
     @Test
     public void qualityDoesNotChangeForOverdueLegendaryItem() {
-        final Item[] items = new Item[] { overdueLegendaryItem() };
-        updateQuality(items);
-        assertEquals(10, items[0].quality);
+        final Item overdueLegendaryItem = overdueLegendaryItem();
+        updateQuality(overdueLegendaryItem);
+        assertEquals(DEFAULT_QUALITY, overdueLegendaryItem.quality);
     }
 
     @Test
     public void qualityDecreasesByTwoForOverdueGenericItem() {
-        final Item[] items = new Item[] { overdueGenericItem(10) };
-        updateQuality(items);
-        assertEquals(8, items[0].quality);
+        final Item overdueGenericItem = overdueGenericItem(DEFAULT_QUALITY);
+        updateQuality(overdueGenericItem);
+        final int expectedQuality = DEFAULT_QUALITY - 2;
+        assertEquals(expectedQuality, overdueGenericItem.quality);
     }
 
     @Test
     public void qualityDoesNotGoBelowZeroForOverdueGenericItem() {
-        final Item[] items = new Item[] { overdueGenericItem(0) };
-        updateQuality(items);
-        assertEquals(0, items[0].quality);
+        final Item overdueGenericItem = overdueGenericItem(MIN_QUALITY);
+        updateQuality(overdueGenericItem);
+        assertEquals(MIN_QUALITY, overdueGenericItem.quality);
     }
 
-    private GildedRose updateQuality(final Item[] items) {
+    private void updateQuality(final Item... items) {
         final GildedRose app = new GildedRose(items);
         app.updateQuality();
-        return app;
     }
 
     private Item backstagePass(final int sellIn, final int quality) {
-        return new Item("Backstage passes to a TAFKAL80ETC concert", sellIn,
-                quality);
+        return new Item(BACKSTAGE_PASS, sellIn, quality);
     }
 
     private Item overdueBackstagePass() {
-        return backstagePass(-1, 10);
+        return backstagePass(SELL_IN_OVERDUE, DEFAULT_QUALITY);
     }
 
     private Item legendaryItem() {
-        return new Item("Sulfuras, Hand of Ragnaros", 20, 10);
+        return new Item(LEGENDARY_ITEM, SELL_IN, DEFAULT_QUALITY);
     }
 
     private Item overdueLegendaryItem() {
-        return new Item("Sulfuras, Hand of Ragnaros", -1, 10);
+        return new Item(LEGENDARY_ITEM, SELL_IN_OVERDUE, DEFAULT_QUALITY);
     }
 
     private Item genericItem(final int quality) {
-        return new Item("generic item", 20, quality);
+        return new Item(GENERIC_ITEM, SELL_IN, quality);
     }
 
     private Item overdueGenericItem(final int quality) {
-        return new Item("generic item", -1, quality);
+        return new Item(GENERIC_ITEM, SELL_IN_OVERDUE, quality);
     }
 
     private Item agedBrie(final int quality) {
-        return new Item("Aged Brie", 20, quality);
+        return new Item(AGED_BRIE, SELL_IN, quality);
     }
 
     private Item overdueAgedBrie(final int quality) {
-        return new Item("Aged Brie", -1, quality);
+        return new Item(AGED_BRIE, SELL_IN_OVERDUE, quality);
     }
 }
