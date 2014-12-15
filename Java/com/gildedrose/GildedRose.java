cf5da5011eee81700d91955e1049aef70af2432a
Guilherme <ggpasqualino@gmail.com>
2014-12-15 15:52:10 +0100

    Removes duplicated conditions

--- Java/com/gildedrose/GildedRose.java
+++ Java/com/gildedrose/GildedRose.java
@@ -17,18 +17,14 @@ class GildedRose {
         if (item.name.equals("Aged Brie")) {
             increaseQuality(item);
         } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1;
+            increaseQuality(item);
 
-                if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.sellIn < 11) {
-                        increaseQuality(item);
-                    }
+            if (item.sellIn < 11) {
+                increaseQuality(item);
+            }
 
-                    if (item.sellIn < 6) {
-                        increaseQuality(item);
-                    }
-                }
+            if (item.sellIn < 6) {
+                increaseQuality(item);
             }
         } else {
             decreaseQuality(item);
