2c5dc7796c192498488207263dab555a69835b46
Guilherme <ggpasqualino@gmail.com>
2014-12-15 15:57:43 +0100

    Changes else blocks to statement

--- Java/com/gildedrose/GildedRose.java
+++ Java/com/gildedrose/GildedRose.java
@@ -26,13 +26,11 @@ class GildedRose {
             if (item.sellIn < 6) {
                 increaseQuality(item);
             }
+        } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            // Do nothing
         } else {
-            if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                // Do nothing
-            } else {
-                if (item.quality > 0) {
-                    item.quality = item.quality - 1;
-                }
+            if (item.quality > 0) {
+                item.quality = item.quality - 1;
             }
         }
 
@@ -47,13 +45,11 @@ class GildedRose {
                 increaseQuality(item);
             } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 zeroQuality(item);
+            } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                // Do nothing
             } else {
-                if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                    // Do nothing
-                } else {
-                    if (item.quality > 0) {
-                        item.quality = item.quality - 1;
-                    }
+                if (item.quality > 0) {
+                    item.quality = item.quality - 1;
                 }
             }
         }
