a9d69401007030eddbbb2d5819fe9a113af49a57
Guilherme <ggpasqualino@gmail.com>
2014-12-15 16:08:41 +0100

    Extracts update methods for each item type

--- Java/com/gildedrose/GildedRose.java
+++ Java/com/gildedrose/GildedRose.java
@@ -15,33 +15,47 @@ class GildedRose {
 
     private void updateQuality(final Item item) {
         if (item.name.equals("Aged Brie")) {
-            increaseQuality(item);
-            decreaseSellIn(item);
-            if (item.sellIn < 0) {
-                increaseQuality(item);
-            }
+            updateAgedBrie(item);
         } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            increaseQuality(item);
-
-            if (item.sellIn < 11) {
-                increaseQuality(item);
-            }
-
-            if (item.sellIn < 6) {
-                increaseQuality(item);
-            }
-            decreaseSellIn(item);
-            if (item.sellIn < 0) {
-                zeroQuality(item);
-            }
+            updateBackstagePass(item);
         } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
             // Do nothing
         } else {
+            updateGenericItem(item);
+        }
+    }
+
+    private void updateAgedBrie(final Item item) {
+        increaseQuality(item);
+        decreaseSellIn(item);
+        if (item.sellIn < 0) {
+            increaseQuality(item);
+        }
+    }
+
+    private void updateBackstagePass(final Item item) {
+        increaseQuality(item);
+
+        if (item.sellIn < 11) {
+            increaseQuality(item);
+        }
+
+        if (item.sellIn < 6) {
+            increaseQuality(item);
+        }
+
+        decreaseSellIn(item);
+
+        if (item.sellIn < 0) {
+            zeroQuality(item);
+        }
+    }
+
+    private void updateGenericItem(final Item item) {
+        decreaseQuality(item);
+        decreaseSellIn(item);
+        if (item.sellIn < 0) {
             decreaseQuality(item);
-            decreaseSellIn(item);
-            if (item.sellIn < 0) {
-                decreaseQuality(item);
-            }
         }
     }
 
