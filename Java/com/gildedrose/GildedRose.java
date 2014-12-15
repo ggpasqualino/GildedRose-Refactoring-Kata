979b0eefad14c0d876fba7bad54a39cf4d263eb8
Guilherme <ggpasqualino@gmail.com>
2014-12-15 16:05:06 +0100

    Meges type conditions

--- Java/com/gildedrose/GildedRose.java
+++ Java/com/gildedrose/GildedRose.java
@@ -16,6 +16,10 @@ class GildedRose {
     private void updateQuality(final Item item) {
         if (item.name.equals("Aged Brie")) {
             increaseQuality(item);
+            decreaseSellIn(item);
+            if (item.sellIn < 0) {
+                increaseQuality(item);
+            }
         } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
             increaseQuality(item);
 
@@ -26,29 +30,15 @@ class GildedRose {
             if (item.sellIn < 6) {
                 increaseQuality(item);
             }
-        } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            // Do nothing
-        } else {
-            decreaseQuality(item);
-        }
-
-        if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            // Do nothing
-        } else {
             decreaseSellIn(item);
-        }
-
-        if (item.name.equals("Aged Brie")) {
-            if (item.sellIn < 0) {
-                increaseQuality(item);
-            }
-        } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
             if (item.sellIn < 0) {
                 zeroQuality(item);
             }
         } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
             // Do nothing
         } else {
+            decreaseQuality(item);
+            decreaseSellIn(item);
             if (item.sellIn < 0) {
                 decreaseQuality(item);
             }
